7d69da0ad79c6460df61abe8735be61378b8a7a8
felipez <tifonzafel@gmail.com>
2020-10-09 19:06:41 +0200

    add test to fetch data
    Signed-off-by: felipez <tifonzafel@gmail.com>

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -7,9 +7,14 @@ import pandas as pd
 from pymongo import MongoClient
 
 from insilicodatabase.interface import (DatabaseConfig, connect_to_db,
+                                        fetch_many_from_collection,
+                                        fetch_one_from_collection,
                                         store_many_in_collection)
 
 from .utils_test import PATH_TEST
+
+DB_NAME = "test_mutations"
+COLLECTION_NAME = "candidates"
 
 
 def add_candidates(mongodb: MongoClient) -> List[int]:
@@ -21,13 +26,13 @@ def add_candidates(mongodb: MongoClient) -> List[int]:
     data.rename(columns={"index": "_id"}, inplace=True)
 
     return store_many_in_collection(
-        mongodb, "candidates", data.to_dict("records"))
+        mongodb, COLLECTION_NAME, data.to_dict("records"))
 
 
 def test_many_insertions():
     """Check that the interface is working."""
     # Connect to the database
-    db_config = DatabaseConfig("test_mutations")
+    db_config = DatabaseConfig(DB_NAME)
     mongodb = connect_to_db(db_config)
 
     expected_ids = {76950, 43380, 26717, 70, 47561, 32800, 37021, 2449, 63555, 72987}
@@ -36,7 +41,22 @@ def test_many_insertions():
         print("received ids: ", ids)
         assert all(index in expected_ids for index in ids)
     finally:
-        collection = mongodb["candidates"]
+        collection = mongodb[COLLECTION_NAME]
         collection.drop()
 
 
+def test_fetch_functions():
+    """Check the fetch functions."""
+    db_config = DatabaseConfig(DB_NAME)
+    mongodb = connect_to_db(db_config)
+    ids = set(add_candidates(mongodb))
+    try:
+        # Fetch indices must be the same than the inserted objects indices
+        data = list(fetch_many_from_collection(mongodb, COLLECTION_NAME, query={}))
+        assert all(entry["_id"] in ids for entry in data)
+        index = ids.pop()
+        one = fetch_one_from_collection(mongodb, COLLECTION_NAME, query={"_id": index})
+        assert index == one["_id"]
+    finally:
+        collection = mongodb[COLLECTION_NAME]
+        collection.drop()
